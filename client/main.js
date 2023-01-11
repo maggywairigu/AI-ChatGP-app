a055b5021eb9f4663f5383227ebb90fdcabb9e19
maggywairigu <wairigumaina2016@gmail.com>
2023-01-11 05:36:09 +0300

    First commit

---
+++ client/main.js
@@ -0,0 +1,134 @@
+//import the picture of the user and the alien from the assets folder.
+import alien from './assets/alienpic.jpg';
+import user from './assets/userg.jpg'
+
+//import the form and container from the html file using the document.query selector
+const form = document.querySelector('form');
+const container = document.querySelector('#chat_container');
+
+let loadInterval;
+
+//function that will load the messages
+//It will return three dots as the alien 'thinks' of an answer
+function loader(element) {
+  //ensures that the element is empty at the start
+  element.textContent = "";
+  //will you the setInterval function to load the three dots
+  loadInterval = setInterval(() => {
+    element.textContent += ".";
+  //if the loading indicator reaches three dots it will reset again to an empty string
+    if (element.textContent === "...."){
+      element.textContent = "";
+    }
+  }, 300)
+}
+
+//function that will make it look like the alien is writing the answers step by step
+function typeText(element, text) {
+  //variable to store the index of the text
+   let index = 0;
+   let interval = setInterval(() => {
+    //Checks if the index is less than length. If true that will mean that the alien still typing
+    if(index < text.length){
+      element.innerHTML += text.charAt(index);
+      index++;
+    }else{
+      clearInterval(interval);
+    }
+   }, 20)
+}
+
+//Generates a unique ID for every message to be able to match overthem.
+function generateID(){
+  //create the date
+   const timeStamp = Date.now();
+   //create a random number
+   const randomNum = Math.random();
+   //covert random number to hexadecimal
+   const hexaDecimalString = randomNum.toString(16);
+
+   return `id-${timeStamp}-${hexaDecimalString}`;
+}
+
+
+//function that will create a chatStripe for either the alien or the user
+function chatStripe (isAlien, value, uniqueID){
+   return(
+    `
+     <div class="wrapper ${isAlien && 'ai'}">
+      <div class="chat">
+       <div class="profile">
+        <img src="${isAlien ? alien : user}" alt="${isAlien ? 'alien' : 'user'}"/>
+       </div>
+       <div class="message" id="${uniqueID}">
+       ${value}
+       <div/>
+      </div>
+     </div>
+    `
+   )
+}
+
+//It will handle submission of your question to the alien
+const handleSubmit = async(e) => {
+  //prevents the browser from reloding after you submit your form
+   e.preventDefault();
+   //It will get the data that you submitted from the form.
+   const data = new FormData(form);
+
+   //Will generate the user chat stripe
+   //We will get into the contain, add its contents to the data that you filled into the form.
+   container.innerHTML += chatStripe(false, data.get('prompt'));
+   //Will reset the text area after you submit inorder to have a clear input
+   form.reset();
+
+
+
+   //alien response stripe
+   const uniqueID = generateID();
+   //The second parameter will be empty because the loader function will be filling the alien chatstrip as it 'thinks'
+   container.innerHTML += chatStripe(true, " ", uniqueID);
+   //As you are typing in the text area, you want to be able to scroll down so that you can see your message if its too long.
+   container.scrollTop = container.scrollHeight;
+
+   //Will help us fetch the message which we initially gave the id='uniqueID' at the chatStripe function
+   const messageDiv = document.getElementById(uniqueID);
+   //Will initalize the loader function that we created in the beggining
+   loader(messageDiv);
+
+   //We will fetch the data from the server and get the aliens response
+   //We will create a new response
+   const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+   })
+ 
+   clearInterval(loadInterval);
+   messageDiv.innerHTML = "";
+
+   if(response.ok){
+     const data = await response.json();
+     const parsedData = data.bot.trim();
+
+     typeText(messageDiv, parsedData);
+
+   }else{
+    const err = await response.text();
+    messageDiv.innerHTML = "Something went wrong";
+    alert(err);
+   }
+}
+
+//Will call the handleSubmit function when you click submit
+form.addEventListener('submit', handleSubmit);
+//Will call the handleSubmit function when press the enter key
+form.addEventListener('keyup', (e) => {
+   if(e.keyCode === 13){
+    handleSubmit(e);
+   }
+})
---
+++ server/server.js
@@ -0,0 +1,58 @@
+import express from 'express';
+//It will allow us to get data from the .env file
+import * as dotenv from 'dotenv';
+//It will allow us to make cross origin requests
+import cors from 'cors';
+import { Configuration, OpenAIApi} from 'openai';
+
+//allows us to use the .env variables
+dotenv.config();
+  
+//configuration functions which accepts the api key as an object
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+//creates an instance of open ai and pass in the configuration we created above
+const openai = new OpenAIApi(configuration);
+
+//Initializes our express application
+const app = express();
+//We are setting up a couple of middlewares
+//allows us to make cross origin requests and allow the server to be called from the frontend 
+app.use(cors());
+//allows us to pass json from the frontend to the backend
+app.use(express.json());
+
+//We are creating a route
+app.get('/', async(req, res) => {
+    res.status(200).send({
+        message: 'Hello from Ayzen'
+    })
+});
+
+//We are creating an app.post route
+//With get route you can't recieve a lot of data from the frontend but the post one allows us to have the body for a lot of data
+app.post('/', async(req, res) => {
+    try {
+        const prompt = req.body.prompt;
+        const response = await openai.createCompletion({
+            model:"text-davinci-003",
+            prompt:`${prompt}`,
+            temperature:0,
+            max_tokens:3000,
+            top_p:1,
+            frequency_penalty:0.5,
+            presence_penalty:0,
+        })
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error })
+    }
+})
+
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
