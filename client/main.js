e028015a0fea2332e4d96b96210f88d471749938
maggywairigu <wairigumaina2016@gmail.com>
2023-01-11 06:44:53 +0300

    Trial

--- client/main.js
+++ client/main.js
@@ -98,7 +98,7 @@ const handleSubmit = async(e) => {
 
    //We will fetch the data from the server and get the aliens response
    //We will create a new response
-   const response = await fetch('https://alien51-code-guru-from-the-galaxies.onrender.com', {
+   const response = await fetch("https://alien51-code-guru-from-the-galaxies.onrender.com", {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
